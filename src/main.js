256d89d6c558eb4b42811e123370a5bbb407d603
itcast-hsian <407775611@qq.com>
2020-04-13 09:52:57 +0800

    路由守卫监控有权限的用户才能进去到页面

--- src/main.js
+++ src/main.js
@@ -17,7 +17,28 @@ Vue.config.productionTip = false
 // 3.注册element ui的组件
 Vue.use(ElementUI);
 
+// 路由守卫
+// to表示要去的那个页面的路由信息
+// from表示是从哪个页面跳转过去的
+// next是个函数，必须要调用才会执行下个页面（类似于express里面的中间件）
+router.beforeEach((to, from, next) => {
+	if (to.path !== "/login") {
+		const userJson = JSON.parse(localStorage.getItem("userInfo")) || {};
+
+		// 需要校验是否有token，并且用户是否是管理员
+		if(userJson.token && userJson.user.role.type === "admin"){
+			next();
+		}else{
+			// 如果不满足上面的条件，就跳转到登录页
+			next("/login");
+		}
+	} else {
+		// 如果是登录页,直接跳转
+		next();
+	}
+})
+
 new Vue({
-  router,
-  render: h => h(App)
+	router,
+	render: h => h(App)
 }).$mount('#app')
